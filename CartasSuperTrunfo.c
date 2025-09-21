063c1fadacd6044622743b305cf744a8aa59182f
cauanzinsx <cauarodrigueskratin69@gmail.com>
2025-09-21 00:32:22 +0000

    novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-    printf (" Desafio Xadrez!\n ");
-    
+  
+  // VARIAVEIS PARA A CARTA 1 ( SAO_PAULO )
 
-  // Área para entrada de dados
+  int populacao_sao_paulo;
+  double area_sao_paulo;
+  double PIB_sao_paulo;
+  int ponto_turistico_sao_paulo;
 
-  // Área para exibição dos dados da cidade
+  // VARIAVEIS PARA A CARTA 2 ( BAHIA )
+
+  int populacao_bahia;
+  double area_bahia;
+  double PIB_bahia;
+  int ponto_turistico_bahia;
+
+  // DADOS PARA A CARTA 1 
+  
+  printf(" --- sao paulo ---\n");
+  printf("digite a população de sao paulo:\n");
+  scanf("%lf, &populacao:\n");
+  printf("digite a area de sao paulo:\n");
+  scanf("%lf, &area:\n");
+  printf("digite o PIB de sao paulo:\n");
+  scanf("%lf, &PIB de sao paulo:\n");
+  printf("ponto turistico de sao paulo: Avenida Paulista\n");
+
+  // DADOS PARA A CARTA 2
+
+  printf(" --- Bahia ---\n");
+  printf("digite a população de Bahia:\n");
+  scanf("%lf, &populacao\n");
+  printf("digite a area de Bahia:\n");
+  scanf("%lf, &area\n");
+  printf("digite o PIB de Bahia:\n");
+  scanf("%lf, &PIB de Bahia:\n");
+  printf("ponto turistico de Bahia:\n");
+
 
 return 0;
 } 
